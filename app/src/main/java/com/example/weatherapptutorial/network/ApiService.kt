2cbc3f64e2a30a3f05d65bc1249ae4a7e18e8a93
coco <riskadesti123@gmail.com>
2023-01-04 13:11:03 +0700

    upload repo

---
+++ app/src/main/java/com/example/weatherapptutorial/network/ApiService.kt
@@ -0,0 +1,15 @@
+package  com.example.weatherapptutorial.network
+
+import com.rivaldo.informasigempa.response.CurrentEarthquakeResponse
+import com.rivaldo.informasigempa.response.HistoryEarthquakeResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService{
+
+    @GET("gempadirasakan.json")
+    fun getHistoryEarthquake() : Call<HistoryEarthquakeResponse>
+
+    @GET("autogempa.json")
+    fun getCurrentEarthquake() : Call<CurrentEarthquakeResponse>
+}
---
+++ app/src/main/java/com/example/weatherapptutorial/utils/Constant.kt
@@ -0,0 +1,7 @@
+package com.riska.informasigempa.utils
+
+class Constant {
+    companion object{
+        const val API_URL = "https://data.bmkg.go.id/DataMKG/TEWS/"
+    }
+}
